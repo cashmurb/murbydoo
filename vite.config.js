5e559b229d2f7d32015912bb81b407a56497b96e
Cashmere Blanche Alin <blanchecashmerebusiness@gmail.com>
2026-08-21 20:14:29 +0800

    fix github pages asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/murbydoo/',
+  base: "/murbydoo/",
   plugins: [react()],
-  server: { host: true, port: 5173 }
-})
+  server: {
+    host: true,
+    port: 5173,
+  },
+});

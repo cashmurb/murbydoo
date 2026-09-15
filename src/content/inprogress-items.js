daf5525e968986891feb53202c437459bf7accb4
Cashmere Blanche Alin <blanchecashmerebusiness@gmail.com>
2026-09-15 22:38:05 +0800

    update in progress content

--- src/content/inprogress-items.js
+++ src/content/inprogress-items.js
@@ -1,5 +1,5 @@
 export const items = [
   { title: 'Home Assistant', subtitle: 'A home automation platform that allows you to control and monitor your home devices.' },
-  { title: 'Coming soon', subtitle: '' },
+  { title: 'Second Brain', subtitle: 'A mini-obsidian-like notes page integrated in my personal website that compiles all my learnings and ideas.' },
   { title: 'Coming soon', subtitle: '' },
 ];

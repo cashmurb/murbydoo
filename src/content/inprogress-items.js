09eea0b1c57c8d9f90f8a2629fb49829e43219ce
Cashmere Blanche Alin <blanchecashmerebusiness@gmail.com>
2026-09-16 23:32:20 +0800

    delete content folder

--- src/content/inprogress-items.js
+++
@@ -1,5 +0,0 @@
-export const items = [
-  { title: 'Home Assistant', subtitle: 'A home automation platform that allows you to control and monitor your home devices.' },
-  { title: 'Second Brain', subtitle: 'A mini-obsidian-like notes page integrated in my personal website that compiles all my learnings and ideas.' },
-  { title: 'Coming soon', subtitle: '' },
-];
